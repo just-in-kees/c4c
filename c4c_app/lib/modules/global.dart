d381c049dd7fc9806735530040d1469b4378941b
Cong Chen <cong.chen@ing.com>
2020-04-10 13:26:52 +0200

    Add second screen for similar transactions #1
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- c4c_app/lib/modules/global.dart
+++ c4c_app/lib/modules/global.dart
@@ -5,20 +5,20 @@ Color darkGreyColor = new Color(0xFF212128);
 TextStyle homeTitles = new TextStyle(
     fontFamily: 'Avenir',
     fontWeight: FontWeight.bold,
-    color: darkGreyColor,
-    fontSize: 40
+    color: Colors.black54,
+    fontSize: 40,
 );
 
 TextStyle transactionDetails = new TextStyle(
     fontFamily: 'Avenir',
     fontWeight: FontWeight.bold,
-    color: darkGreyColor,
+    color: Colors.black54,
     fontSize: 20
 );
 
 TextStyle transactionDate = new TextStyle(
     fontFamily: 'Avenir',
     fontWeight: FontWeight.bold,
-    color: darkGreyColor,
+    color: Colors.black54,
     fontSize: 10
 );
