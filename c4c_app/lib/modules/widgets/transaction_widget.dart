921fd2bc8d32921976d75c7bce22d021b9cbb000
Cong Chen <cong.chen@ing.com>
2020-04-17 15:29:21 +0200

    Add stylish for similar transactions #1
    Signed-off-by: Cong Chen <cong.chen@ing.com>

--- c4c_app/lib/modules/widgets/transaction_widget.dart
+++ c4c_app/lib/modules/widgets/transaction_widget.dart
@@ -53,21 +53,18 @@ class HomeTransaction extends StatelessWidget {
                                   style: transactionDetails,
                                 ),
                                 Text(
-                                  DateTime
-                                      .fromMillisecondsSinceEpoch(
-                                      snapshot.data.date)
-                                      .day
-                                      .toString() +
+                                  DateTime.fromMillisecondsSinceEpoch(
+                                              snapshot.data.date)
+                                          .day
+                                          .toString() +
                                       "/" +
-                                      DateTime
-                                          .fromMillisecondsSinceEpoch(
-                                          snapshot.data.date)
+                                      DateTime.fromMillisecondsSinceEpoch(
+                                              snapshot.data.date)
                                           .month
                                           .toString() +
                                       "/" +
-                                      DateTime
-                                          .fromMillisecondsSinceEpoch(
-                                          snapshot.data.date)
+                                      DateTime.fromMillisecondsSinceEpoch(
+                                              snapshot.data.date)
                                           .year
                                           .toString(),
                                   style: transactionDetails,
@@ -84,12 +81,15 @@ class HomeTransaction extends StatelessWidget {
                                   child: Icon(Icons.show_chart, size: 50),
                                   backgroundColor: Colors.amber,
                                   onPressed: () {
-                                    Future<List<Transaction>> transactionList = fetchTransactionList();
+                                    Future<List<Transaction>> transactionList =
+                                        fetchTransactionList();
                                     Navigator.push(
                                       context,
                                       MaterialPageRoute(
                                           builder: (context) =>
-                                              SimilarTransactionScreen(snapshot.data.description,transactionList)),
+                                              SimilarTransactionScreen(
+                                                  snapshot.data.description,
+                                                  transactionList)),
                                     );
                                   },
                                 )
@@ -113,7 +113,6 @@ class HomeTransaction extends StatelessWidget {
 }
 
 class SimilarTransactionScreen extends StatelessWidget {
-
   final String transactionDescription;
   final Future<List<Transaction>> transactionList;
 
@@ -121,9 +120,6 @@ class SimilarTransactionScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
-
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.orange,
@@ -140,63 +136,85 @@ class SimilarTransactionScreen extends StatelessWidget {
               if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
-
-              if (snapshot.hasData){
+              if (snapshot.hasData) {
                 List<Transaction> similarTransactions = [];
                 List<Transaction> transactions = snapshot.data;
-                for (var transaction in transactions){
-                  if (transactionDescription==transaction.description) {
-                    print(identical(transaction.description, transaction.amount));
+                for (var transaction in transactions) {
+                  if (transactionDescription == transaction.description) {
+                    print(
+                        identical(transaction.description, transaction.amount));
                     similarTransactions.add(transaction);
                   }
                 }
                 return ListView.builder(
+
+                    shrinkWrap: false,
+                    scrollDirection: Axis.vertical,
                     itemCount: similarTransactions.length,
                     itemBuilder: (BuildContext context, index) {
                       return new ListTile(
-                          title: Padding(
-
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
+                        contentPadding: EdgeInsets.symmetric(
+                            horizontal: 15.0, vertical: 10.0),
+                        title: Container(
+                            margin: new EdgeInsets.symmetric(horizontal: 1.0, vertical: 1.0),
+                            decoration: BoxDecoration(color: Colors.orange,
+                                borderRadius: new BorderRadius.only(
+                                    topLeft:  const  Radius.circular(7.0),
+                                    topRight: const  Radius.circular(7.0),
+                                    bottomLeft:  const  Radius.circular(7.0),
+                                    bottomRight: const  Radius.circular(7.0))
+                            ),
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    similarTransactions[index].description,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Row(children: <Widget>[
+                                    Icon(Icons.euro_symbol,
+                                        color: Colors.white),
                                     Text(
-                                      similarTransactions[index].description,
-// Cuts of the text and shows ...
-                                      overflow: TextOverflow.ellipsis,
-
+                                      similarTransactions[index]
+                                          .amount
+                                          .toString(),
                                     ),
-                                    Text(
-                                      similarTransactions[index].amount.toString(),
-
-                                    ),
-                                    Text(
-                                      DateTime
-                                          .fromMillisecondsSinceEpoch(
-                                          similarTransactions[index].date)
-                                          .day
-                                          .toString() +
-                                          "/" +
-                                          DateTime
-                                              .fromMillisecondsSinceEpoch(
-                                              similarTransactions[index].date)
-                                              .month
-                                              .toString() +
-                                          "/" +
-                                          DateTime
-                                              .fromMillisecondsSinceEpoch(
-                                              similarTransactions[index].date)
-                                              .year
-                                              .toString(),
-                                    ),
-                                  ]))
+                                  ]),
+                                  Row(
+                                    children: <Widget>[
+                                      Icon(Icons.date_range,
+                                          color: Colors.white),
+                                      Text(
+                                        DateTime.fromMillisecondsSinceEpoch(
+                                                    similarTransactions[index]
+                                                        .date)
+                                                .day
+                                                .toString() +
+                                            "/" +
+                                            DateTime.fromMillisecondsSinceEpoch(
+                                                    similarTransactions[index]
+                                                        .date)
+                                                .month
+                                                .toString() +
+                                            "/" +
+                                            DateTime.fromMillisecondsSinceEpoch(
+                                                    similarTransactions[index]
+                                                        .date)
+                                                .year
+                                                .toString(),
+                                      ),
+                                    ],
+                                  ),
+                                ])),
                       );
-                    }
-                );
+                    });
               }
-            }
-        )
-    );
+            }));
   }
 }
 
@@ -219,7 +237,7 @@ class MyStatelessWidget extends StatelessWidget {
           RaisedButton(
             onPressed: () => fetchTransaction(10),
             child:
-            const Text('Other Transaction', style: TextStyle(fontSize: 20)),
+                const Text('Other Transaction', style: TextStyle(fontSize: 20)),
           ),
           const SizedBox(height: 30),
           trans(10),
@@ -230,7 +248,8 @@ class MyStatelessWidget extends StatelessWidget {
 }
 
 Future<List<Transaction>> fetchTransactionList() async {
-  final token = '';
+  final token =
+      'eyJhbGciOiJFUzI1NiIsImtpZCI6IjdjODEwNTA4LWRlNGYtNGY1OC1iZTdlLTAyZmNkZjY2NDJkYiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcxMzI3NTksImlhdCI6MTU4NzEyNTU1OSwiaXNzIjoidGluazovL2F1dGgiLCJqdGkiOiI0NmQzNTFkYi1hNzQyLTQyY2EtOTZhZS01ODJkNzc3MTJmOTIiLCJvcmlnaW4iOiJtYWluIiwic2NvcGVzIjpbImludmVzdG1lbnRzOnJlYWQiLCJ1c2VyOnJlYWQiLCJzdGF0aXN0aWNzOnJlYWQiLCJhY2NvdW50czpyZWFkIiwidHJhbnNhY3Rpb25zOnJlYWQiXSwic3ViIjoidGluazovL2F1dGgvdXNlci9jZDU2YTZiOTk0Y2I0ZWY0OGRmYWE0MjlmMmZkZWRkNSIsInRpbms6Ly9hcHAvaWQiOiJmMmQ2NDQxZWM0OTQ0NTdmYjEwN2NhODIxNzdiYjhjYiJ9.hKAeE3W6nsyo1Og73hV7x8kRsLIUnT6oSfI9Gnt5HsbyPHQt1ZYBA0F_-L3nwhrXWaXyhA-PF0y9nrNVGvpiag';
 
   final response = await http.get(
     'https://api.tink.com/api/v1/transactions/',
@@ -244,9 +263,7 @@ Future<List<Transaction>> fetchTransactionList() async {
     // then parse the JSON.
     final responseJson = json.decode(response.body);
 
-    return (responseJson as List)
-        .map((p) => Transaction.fromJson(p))
-        .toList();
+    return (responseJson as List).map((p) => Transaction.fromJson(p)).toList();
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -256,7 +273,8 @@ Future<List<Transaction>> fetchTransactionList() async {
 }
 
 Future<Transaction> fetchTransaction(int index) async {
-  final token = 'eyJhbGciOiJFUzI1NiIsImtpZCI6IjRlOTFmZTQ5LWE4MWMtNDlmMi1hNmVkLWQ0NWZmMGYxZmU0NSIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcxMjA0ODAsImlhdCI6MTU4NzExMzI4MCwiaXNzIjoidGluazovL2F1dGgiLCJqdGkiOiIyMjFkYWY4Zi1jZDNiLTRmMTYtYmRjYS04OGU5Mjk3OGVjYWIiLCJvcmlnaW4iOiJtYWluIiwic2NvcGVzIjpbImludmVzdG1lbnRzOnJlYWQiLCJ1c2VyOnJlYWQiLCJzdGF0aXN0aWNzOnJlYWQiLCJhY2NvdW50czpyZWFkIiwidHJhbnNhY3Rpb25zOnJlYWQiXSwic3ViIjoidGluazovL2F1dGgvdXNlci80OTNjOTQxMGFkY2Y0ZTA1OWE2YmE3NzljMWI0YzQ1MSIsInRpbms6Ly9hcHAvaWQiOiJmMmQ2NDQxZWM0OTQ0NTdmYjEwN2NhODIxNzdiYjhjYiJ9.ai0_tztRt9DM2NFqcLptVW-rJBqiiWmBqdEiA3KCZtCB5xokmloCeHBXBAd6271pbjsdgdvfuNUoq_Tquk2SJQ';
+  final token =
+      'eyJhbGciOiJFUzI1NiIsImtpZCI6IjdjODEwNTA4LWRlNGYtNGY1OC1iZTdlLTAyZmNkZjY2NDJkYiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcxMzI3NTksImlhdCI6MTU4NzEyNTU1OSwiaXNzIjoidGluazovL2F1dGgiLCJqdGkiOiI0NmQzNTFkYi1hNzQyLTQyY2EtOTZhZS01ODJkNzc3MTJmOTIiLCJvcmlnaW4iOiJtYWluIiwic2NvcGVzIjpbImludmVzdG1lbnRzOnJlYWQiLCJ1c2VyOnJlYWQiLCJzdGF0aXN0aWNzOnJlYWQiLCJhY2NvdW50czpyZWFkIiwidHJhbnNhY3Rpb25zOnJlYWQiXSwic3ViIjoidGluazovL2F1dGgvdXNlci9jZDU2YTZiOTk0Y2I0ZWY0OGRmYWE0MjlmMmZkZWRkNSIsInRpbms6Ly9hcHAvaWQiOiJmMmQ2NDQxZWM0OTQ0NTdmYjEwN2NhODIxNzdiYjhjYiJ9.hKAeE3W6nsyo1Og73hV7x8kRsLIUnT6oSfI9Gnt5HsbyPHQt1ZYBA0F_-L3nwhrXWaXyhA-PF0y9nrNVGvpiag';
   final response = await http.get(
     'https://api.tink.com/api/v1/transactions/',
     headers: {
@@ -358,46 +376,47 @@ class Transaction {
 
   final bool userModified;
 
-  Transaction({this.accountId,
-    this.amount,
-    this.categoryId,
-    this.categoryType,
-    this.credentialsId,
-    this.date,
-    this.description,
-    this.formattedDescription,
-    this.id,
-    this.inserted,
-    this.internalPayload,
-    this.lastModified,
-    this.merchantId,
-    this.notes,
-    this.originalAmount,
-    this.originalDate,
-    this.originalDescription,
-    this.payload,
-    this.MESSAGE,
-    this.TRANSFER_ACCOUNT_NAME_EXTERNAL,
-    this.DETAILS,
-    this.pending,
-    this.timestamp,
-    this.type,
-    this.userId,
-    this.upcoming,
-    this.userModifiedAmount,
-    this.userModifiedCategory,
-    this.userModifiedDate,
-    this.userModifiedDescription,
-    this.userModifiedLocation,
-    this.currencyDenominatedAmount,
-    this.unscaledValue,
-    this.scale,
-    this.currencyCode,
-    this.currencyDenominatedOriginalAmount,
-    this.parts,
-    this.partnerPayload,
-    this.dispensableAmount,
-    this.userModified});
+  Transaction(
+      {this.accountId,
+      this.amount,
+      this.categoryId,
+      this.categoryType,
+      this.credentialsId,
+      this.date,
+      this.description,
+      this.formattedDescription,
+      this.id,
+      this.inserted,
+      this.internalPayload,
+      this.lastModified,
+      this.merchantId,
+      this.notes,
+      this.originalAmount,
+      this.originalDate,
+      this.originalDescription,
+      this.payload,
+      this.MESSAGE,
+      this.TRANSFER_ACCOUNT_NAME_EXTERNAL,
+      this.DETAILS,
+      this.pending,
+      this.timestamp,
+      this.type,
+      this.userId,
+      this.upcoming,
+      this.userModifiedAmount,
+      this.userModifiedCategory,
+      this.userModifiedDate,
+      this.userModifiedDescription,
+      this.userModifiedLocation,
+      this.currencyDenominatedAmount,
+      this.unscaledValue,
+      this.scale,
+      this.currencyCode,
+      this.currencyDenominatedOriginalAmount,
+      this.parts,
+      this.partnerPayload,
+      this.dispensableAmount,
+      this.userModified});
 
   factory Transaction.fromJson(Map<String, dynamic> json) {
     return Transaction(
@@ -437,7 +456,7 @@ class Transaction {
       scale: json['scale'],
       currencyCode: json['currencyCode'],
       currencyDenominatedOriginalAmount:
-      json['currencyDenominatedOriginalAmount'],
+          json['currencyDenominatedOriginalAmount'],
       parts: json['parts'],
       partnerPayload: json['partnerPayload'],
       dispensableAmount: json['dispensableAmount'],
